f0bdb6e3f7319203829e760e40b0f2fbcbda757d
risa06 <risamahardika89@gmail.com>
2020-11-08 21:26:59 +0700

    second commit

--- app/src/main/java/com/prakpapb1/tabnavigation/FirstFragment.java
+++ app/src/main/java/com/prakpapb1/tabnavigation/FirstFragment.java
@@ -3,62 +3,72 @@ package com.prakpapb1.tabnavigation;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FirstFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class FirstFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ArrayList<ItemData> itemValues;
+    private RecyclerView recyclerView;
+    private ItemAdapter itemAdapter;
+    private FloatingActionButton addButton;
 
     public FirstFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirstFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FirstFragment newInstance(String param1, String param2) {
-        FirstFragment fragment = new FirstFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view=inflater.inflate(R.layout.fragment_first, container, false);
+        recyclerView=view.findViewById(R.id.recyclerView);
+        addButton=view.findViewById(R.id.addButton);
+
+        itemValues=new ArrayList<>();
+        for (int i=1; i<=10; i++){
+            ItemData item=new ItemData();
+            item.itemTitle="Title Data - "+i;
+            item.itemSubTitle="Subtitle Data - "+i;
+            itemValues.add(item);
+        }
+        itemAdapter=new ItemAdapter(getActivity(), itemValues);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(getActivity(),
+                LinearLayoutManager.HORIZONTAL, false);
+        GridLayoutManager layoutManager2=new GridLayoutManager(getActivity(), 2);
+        recyclerView.setLayoutManager(layoutManager2);
+        recyclerView.setAdapter(itemAdapter);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addData();
+            }
+        });
+
+        return view;
+    }
+
+    private void addData() {
+        ItemData item=new ItemData();
+        item.itemTitle="Title Data - Baru";
+        item.itemSubTitle="Subtitle Data - Baru";
+        itemValues.add(item);
+        itemAdapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/prakpapb1/tabnavigation/ItemAdapter.java
+++ app/src/main/java/com/prakpapb1/tabnavigation/ItemAdapter.java
@@ -1,12 +1,54 @@
 package com.prakpapb1.tabnavigation;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ItemAdapter {
+import java.util.ArrayList;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<ItemData> values;
+    private LayoutInflater inflater;
+
+    public ItemAdapter(Context context, ArrayList<ItemData> values){
+        this.context=context;
+        this.values=values;
+        this.inflater=LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view=inflater.inflate(R.layout.item_list,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final ItemData data=values.get(position);
+        holder.titleText.setText(data.itemTitle);
+        holder.subText.setText(data.itemSubTitle);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Anda memilih data "+data.itemTitle,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return values.size();
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView titleText;
